1d461ca936e461f6ced1ae3521c2a70868f01d7e
RanRai <ranno.raidla@tktk.ee>
2023-02-01 20:24:52 +0200

    add week4loops code

---
+++ ForLoopSum/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+int sum = 0;
+
+for(int i = 1; i < 4; i++) //i väärtus on 0(null) ja i-- = i-1
+{
+    Console.WriteLine($"i = {i}"); //siin on kuvatavad väärtused
+    Console.WriteLine($"Current total: {sum}");
+    sum = sum + i; //sum += 1;
+}
+//Console.WriteLine($"Final total {sum}");
+
---
+++ RandomNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Random rnd= new Random();
+
+int myRandomNum;
+
+int randomSum = 0;
+
+for(int i = 0; i < 3; i++)
+{
+    myRandomNum= rnd.Next(0, 11);//algpunkt sisse arvatud, lõpp-punkt jäetakse välja
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+
+Console.WriteLine($"Random sum total: {randomSum}");
+
+
+//int myRandomNum = rnd.Next(0, 11); Sel juhul genereerib ühe
+//Console.WriteLine($"My random number is: {myRandomNum}"); numbri vahemikus1-10
